4c5581c0abca76b6e665070b17ca8fb5155fd293
CMichael <cmichael1@ymail.com>
2016-06-19 12:47:54 -0700

    Update Mirror_HTML5_Video_Horizontally 2.js

--- Mirror_HTML5_Video_Horizontally 2.js
+++ Mirror_HTML5_Video_Horizontally 2.js
@@ -9,4 +9,12 @@
   {
     node.innerHTML = str;
   }
+    cssText =
+     ".xflip { \
+      -moz-transform:    scale(-1, 1) !important;\
+      -webkit-transform: scale(-1, 1) !important;\
+      -o-transform:      scale(-1, 1) !important;\
+      transform:         scale(-1, 1) !important;\
+      filter: FlipH !important;\
+    }";
 })();
