b73144f72fc8a1941a3359733198a4109edbca48
CMichael <cmichael1@ymail.com>
2016-06-19 13:28:41 -0700

    Update Mirror_HTML5_Video_Vertically-old.js

--- Mirror_HTML5_Video_Vertically-old.js
+++ Mirror_HTML5_Video_Vertically-old.js
@@ -28,10 +28,10 @@
     var video;
     for(var i = 0; i < numberOfVideos; ++i)
     {
-      if(document.getElementsByTagName("video")[i].videoHeight > 100)
+        if(document.getElementsByTagName("video")[i].videoHeight > 100)
       {
-        video = document.getElementsByTagName("video")[i];
-        video.classList.toggle('xflip');
+          video = document.getElementsByTagName("video")[i];
+          video.classList.toggle('xflip');
         break;
       }
     }
